0e5136a05cd27e282440e89abd0e7a14d6f33e01
ana.laclau <ana.laclau@mediamonks.com>
2022-08-08 10:13:34 -0300

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Calculator} from './src/screens/Calculator';
+
+export const App = () => {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/Calculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+export const Calculator = () => {
+  return (
+    <SafeAreaView style={styles.background}>
+      <Text style={styles.text}>hola</Text>
+    </SafeAreaView>
+  );
+};
