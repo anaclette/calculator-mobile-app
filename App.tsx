4d3102017029cbc70ec106b5d975b6d0a481d51a
ana.laclau <ana.laclau@mediamonks.com>
2022-08-09 09:53:03 -0300

    Added status bar to app file

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {Calculator} from './src/screens/Calculator';
 
 export const App = () => {
   return (
     <>
+      <StatusBar networkActivityIndicatorVisible barStyle="light-content" />
       <Calculator />
     </>
   );
